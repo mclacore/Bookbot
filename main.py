4320ede23e8a3ff70c08957f507266b5404a8644
mclacore <mclacore@gmail.com>
2024-04-09 14:40:45 -0700

    Creating new dict for each letter

--- main.py
+++ main.py
@@ -3,11 +3,12 @@ def main():
     book_path = "books/frankenstein.txt"
     with open(book_path) as f:
         book_content = f.read()
-        
+
     # counts the words
     word_count = len(book_content.split())
 
     # counts the letters
+    dict_list = []
     letter_dict = {}
     for letters in book_content:
         lower_words = letters.lower()
@@ -15,10 +16,21 @@ def main():
             letter_dict[lower_words] += 1
         else:
             letter_dict[lower_words] = 1
+    
+    # create dict for each letter
+    for char in letter_dict:
+        new_dict = {}
+        count = letter_dict[char]
+        new_dict[char] = count
+        if char.isalpha():
+            dict_list.append(new_dict)
+
 
     # returns
-    print(letter_dict)
-    print(f"Book has {word_count} words.")
-
+    print(f"--- Begin report of {book_path} ---")
+    print(f"{word_count} words found in this book.")
+    print(dict_list)
+    # print(f"The letter {letter_dict[0]} was found {letter_dict[1]} times.")
+    print(f"--- End report ---")
 
 main()
